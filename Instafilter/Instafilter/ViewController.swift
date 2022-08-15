f953f64288740bb436e586931b5e4065e70b46f6
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-08-15 16:02:13 +0500

    Instafilter is done and challenge completed

---
+++ Instafilter/Instafilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instafilter
+//
+//  Created by Павел Кай on 15.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
