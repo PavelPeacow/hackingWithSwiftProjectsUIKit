505349ef37056870856a0dce730bf98fbfdc5d12
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-08-16 14:25:36 +0500

    Animation is done and challenge completed

---
+++ Animation/Animation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Павел Кай on 16.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
