0847a604378d5a2e4c18ddfe21d636e1d28cd46f
PavelPeacow <68381498+PavelPeacow@users.noreply.github.com>
2022-08-19 15:47:48 +0500

    Debugging is done and challenge completed

---
+++ Debugging/Debugging/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Debugging
+//
+//  Created by Павел Кай on 19.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("number \(i)")
+        }
+        
+    }
+
+
+}
+
